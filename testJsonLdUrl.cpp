253365af757b99d192ae80bcece57f2b77e29ae4
Your Name <you@example.com>
2015-04-02 02:33:28 +0200

    working on JsonLdUrl.h

---
+++ testJsonLdUrl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+//#include <boost/regex.hpp>
+//#include <boost/regex/icu.hpp>
+#include <regex>
+
+using namespace std;
+
+//typedef std::wbasic_regex<std::wstring> Pattern;
+
+//const wchar_t xxx[6] = L"[a-\xEFFFF]";
+
+//std::wregex pattern(xxx);
+
+
+int main()
+{
+  std::string s = "Boost Libraries";
+  regex expr{"\\w+\\s\\w+"};
+  std::cout << std::boolalpha << regex_match(s, expr) << '\n';
+}
