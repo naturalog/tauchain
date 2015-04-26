af1257a0ee43486652f469b3b17f936b3bef99c5
naturalog <ohad@idni.org>
2015-04-26 10:09:59 +0300

    restyle only

--- ppjson.cpp
+++
@@ -1,9 +0,0 @@
-#include "json_spirit.h"
-#include <iostream>
-
-int main() {
-	json_spirit::mValue v;
-	json_spirit::read_stream ( std::cin, v );
-	std::cout << json_spirit::write_string ( v, json_spirit::pretty_print | json_spirit::single_line_arrays ) << std::endl;
-	return 0;
-}
