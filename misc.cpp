4c9942e7978edd7b898c93142513106bbad41d8c
naturalog <ohad@idni.org>
2015-04-30 22:39:12 +0300

    fix crash

--- misc.cpp
+++ misc.cpp
@@ -4,6 +4,7 @@ bidict& dict = *new bidict;
 bool deref = true, shorten = false;
 
 bidict::bidict() {
+	set("GND");
 }
 
 void bidict::set ( const vector<string>& v ) {
@@ -13,7 +14,7 @@ void bidict::set ( const vector<string>& v ) {
 int bidict::set ( const string& v ) {
 	auto it = m.right.find ( v );
 	if ( it != m.right.end() ) return it->second;
-	int k = m.size() + 1;
+	int k = m.size();
 	if ( v[0] == '?' || v[0] == '_' ) k = -k;
 	m.insert ( bm::value_type ( k, v ) );
 	return k;
