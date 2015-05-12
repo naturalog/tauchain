02828d81c2d2662bea93bec33341bfb05397c0cb
recycled consciousness <you@example.com>
2015-05-12 19:50:14 +0200

    marpa pseudocode3

--- marpa.cpp
+++ marpa.cpp
@@ -4,14 +4,134 @@
 const string pmatches = "http://www.w3.org/2000/10/swap/grammar/bnf#matches";
 const string pmbos = "http://www.w3.org/2000/10/swap/grammar/bnf#mustBeOneSequence";
 
-        typedef int earleme;
-        typedef int sym;
-        typedef int rule;
-        typedef int event_type;
-        typedef int rank;
+typedef Marpa_Symbol_ID sym;
+typedef Marpa_Rule_ID rule;
+typedef size_t pos;
 
 struct Grammar{
+	uint num_syms = 0;
 	Marpa_Grammar g;
+	map<sym,regex> regexes;
+	map<sym,string> names;
+	map<pos,size_t> lengths;
+	
+	parse (filename, grammar)
+	{
+
+		sym start = add(grammar["document"]);
+		start_symbol_set(start);
+		precompute();
+		Marpa_Recognizer r = marpa_r_new(g);
+		check_null(r);
+		marpa_r_start_input(r);
+
+
+
+		
+
+
+
+		inp = read(filename)
+		for(pos p = 0; p != inp.size();;)
+		{
+		
+			//lexing:just find the longest match?
+			regex:match m
+			sym s;
+			for (auto r = regexes.begin(); r != regexes.end(); r++)
+				m2 = r.second.match(inp)
+				if !m.valid or m.length > m2.length
+					m = m2
+					s = r.first
+					
+			if !m.valid die brutally;
+			
+			int err = marpa_r_alternative(r, s, pos, 1);
+			lengths[pos] = m.lenght();
+			
+			#Return value: On success, MARPA_ERR_NONE. On failure, some other error code.
+			if (r == MARPA_ERR_UNEXPECTED_TOKEN_ID)
+			{
+				sym[sym_names.size()] expected;
+				num_expected = check_int(marpa_r_terminals_expected(r, sym));
+				dout << "expecting:";
+				//for (int i = 0; i < num_expected; i++)
+				dout << endl;
+				error();
+			}		
+			else if (r != MARPA_ERR_NONE)
+				error();
+			check_int(marpa_r_earleme_complete(r));
+		}
+
+
+
+
+
+		Marpa_Bocage b = marpa_b_new(r, -1);
+		check_null(b);
+		Marpa_Order o = marpa_o_new(b);
+		check_null(o);
+		Marpa_Tree t = marpa_t_new(o);
+		check_null(t);
+		check_int(marpa_t_next(t));
+		Marpa_Value marpa_v_new(t);
+		check_null(v);
+
+
+
+		//"valuator" loop. marpa tells us what rules with what child nodes or tokens were matched, we build the ast with our "values"
+
+
+		while(1)
+		{
+			Marpa_Step_Type st = check_int(marpa_v_step(v));
+			if (st == MARPA_STEP_INACTIVE) break;
+			switch(st)
+			{
+				case MARPA_STEP_TOKEN:
+					sym id = marpa_v_symbol(v);
+					
+					obj x;
+					x["name"] = names[id];
+					
+					pos p = marpa_v_token_value(v);
+					stack[marpa_v_result(v)] = input[p:lengths[p]]
+					
+					break;
+				case MARPA_STEP_RULE:
+					
+			
+			
+
+	}
+
+
+
+
+
+	//create marpa symbols and rules from grammar description in rdf
+	sym add(obj x)
+	{
+		s = symbol_new();
+		if (x.has(pmbos))
+			//mbos is supposed to be a list of lists
+			for i in x[pmbos]
+				rule_new(x, add_list(i))
+		elif x.has(pmatches)
+			regexes[s] = x[pmatches];
+		return s;
+	}
+
+	add_list(list x)
+	{
+		s = symbol_new();
+		r = rule_new(s, [add(i) for i in x])
+		return s
+	}
+
+	
+	
 	Grammar()
 	{	
 		if (marpa_check_version (MARPA_MAJOR_VERSION ,MARPA_MINOR_VERSION, MARPA_MICRO_VERSION ) != MARPA_ERR_NONE)
@@ -47,17 +167,10 @@ struct Grammar{
 
 	sym symbol_new()
 	{
+		num_syms++;
 		return check_int(marpa_g_symbol_new(g));
 	}
-	
-	/*
-	bool symbol_is_accessible(s, sym):
-		assert type(sym) == symbol_int
-		r = s.check_int(lib.marpa_g_symbol_is_accessible(s.g, sym))
-		assert r != -1
-		return r
-	*/	
-	
+
 	rule_id rule_new(sym lhs, syms rhs) {
             return check_int(marpa_g_rule_new(g, lhs, rhs, rds.size()));
         }
@@ -72,168 +185,37 @@ struct Grammar{
 		print_events();
 	}
 
-	def events(s):
-		count = s.check_int(lib.marpa_g_event_count(s.g))
-		log('%s events'%count)
-		if count > 0:
-			result = ffi.new('Marpa_Event*')
-			for i in range(count):
-				event_type = s.check_int(lib.marpa_g_event(s.g, result, i))
-				event_value = result.t_value
-				r = event_type, event_value
-				log((i,r))
-				yield r
-
-	def print_events(s):
-		for dummy in s.events():
-			pass
+	void print_events()
+	{
+		int count = check_int(marpa_g_event_count(g));
+		dout << '%s events'%count
+		if (count > 0)
+		{
+			Marpa_Event e;
+			for (int i = 0; i < count; i++)
+			{
+				int etype = check_int(marpa_g_event(g, &e, i));
+				dout << etype << ", " << e.t_value;
+			}
+		}
+	}
 
 	void start_symbol_set(sym_id s){
 		check_int( marpa_g_start_symbol_set(g, s) );
 	}
 
-	void error(int s){
+	void error(){
 		int e = marpa_g_error(g, NULL);
 		throw(new exception(e + ": " + errors[e]));
 	}
-
-def log_error(e):
-	err = codes.errors[e]
-	log(err[0] + " " + repr(err[1]) )#constant + description
-
-class Recce(object):
-	def __init__(s, g):
-		s.g = g
-		s.r = s.g.check_null(lib.marpa_r_new(g.g))
-		log(s.r)
-	def __del__(s):
-		lib.marpa_r_unref(s.r)
-	def start_input(s):
-		s.g.check_int(lib.marpa_r_start_input(s.r))
-
-	def alternative(s, sym, val, length=1):
-		assert type(sym) == symbol_int
-		assert type(val) == int
-		
-		r = lib.marpa_r_alternative(s.r, sym, val, length)
-		#Return value: On success, MARPA_ERR_NONE. On failure, some other error code.
-		if r != lib.MARPA_ERR_NONE:
-			log_error(r)
-
-
-	def earleme_complete(s):
-		s.g.check_int(lib.marpa_r_earleme_complete(s.r))
-	def latest_earley_set(s):
-		return lib.marpa_r_latest_earley_set(s.r)
-
-class Bocage(object):
-	def __init__(s, r, earley_set_ID):
-		s.g = r.g
-		s.b = s.g.check_null(lib.marpa_b_new(r.r, earley_set_ID))
-		log(s.b)
-	def __del__(s):
-		lib.marpa_b_unref(s.b)
-
-class Order(object):
-	def __init__(s, bocage):
-		s.g = bocage.g
-		s.o = s.g.check_null(lib.marpa_o_new(bocage.b))
-		log(s.o)
-	def __del__(s):
-		lib.marpa_o_unref(s.o)
-
-class Tree(object):
-	def __init__(s, order):
-		s.g = order.g
-		s.t = s.g.check_null(lib.marpa_t_new(order.o))
-		log(s.t)
-	def __del__(s):
-		lib.marpa_t_unref(s.t)
-	def nxt(s):
-		while True:
-			r = s.g.check_int(lib.marpa_t_next(s.t))
-			if r == -1:
-				break
-			yield r
-
-class Valuator(object):
-	def __init__(s, tree):
-		s.g = tree.g
-		s.v = s.g.check_null(lib.marpa_v_new(tree.t))
-		log(s.v)
-	def __del__(s):
-		if s.v != None:
-			s.unref()
-	def unref(s):
-		lib.marpa_v_unref(s.v)
-		s.v = None
-	def step(s):
-		assert s.v
-		return s.g.check_int(lib.marpa_v_step(s.v))
-		
-
-
-
-
-
-
-
-map<int,regex> regexes
-
-parsed (result, filename, grammar)
-{
-
-	start = symbol_new();
-	add(grammar["document"])
-	
-	
-
-	inp = read(filename)
-	for(pos = inp.begin(); pos != inp.end();;)
-	
-		//lexing:just find longest match?
-		regex:match m
-		int symbol_id;
-		for (auto r = regexes.begin(); r != regexes.end(); r++)
-			m2 = r.second.match(inp)
-			if !m.valid or m.length > m2.length
-				m = m2
-				symbol_id = r.first
-		if !m.valid die brutally;
-		
-		
-		alternative(symbol_id)
-		earleme_complete()
-		
-	bocage, order, tree, valuator..
-	
-	
-	
-		in what form do we want the output?..
-
-
-//create marpa grammar
-add(obj x)
-{
-	s = symbol_new();
-	if x.has(pmbos)
-		for i in x[pmbos]
-			rule_new(x, add_list(i))
-	elif x.has(pmatches)
-		regexes[s] = new regex (x[pmatches]).compile()
-	return s
-}	
-			
-add_list(list x)
-{
-	s = symbol_new();
-	r = rule_new(s, [add(i) for i in x])
-	return s
 }
-	
-	
-	
-	
+
+
+
+
+
+
+
 	
 	
 	
