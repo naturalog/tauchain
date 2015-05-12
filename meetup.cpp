2c8ca0beaa8669d42ca71aa5e1c8f8de64fb5b62
naturalog <ohad@idni.org>
2015-05-12 04:59:20 +0300

    meetup n3 ver

--- meetup.cpp
+++ meetup.cpp
@@ -6,44 +6,58 @@
 #include <vector>
 using namespace std;
 // http://listofrandomnames.com",
-vector<string> names = { "Enrique", "Arianne", "Muriel", "Santina", "Miguel", "Easter", "Arla", "Karisa", "Bailey", "Brice", "Della", "Pamala", "Mikel", "Roselyn", "Malisa", "Tommye", "Lucien", "Norma", "Fidela", "Loralee", "Waylon", "Ching", "Dewitt", "Saturnina", "Russel", "Helaine", "Gena", "Darlena", "Julianna", "Marylin", "Blanca", "Amee", "Adina", "Ashlea", "Louanne", "Merlyn", "Min", "Vivian", "Jaquelyn", "Mitchell", "Ka", "Suellen", "Jefferey", "Nicolette", "German", "Nisha", "Justine", "Debra", "Alycia", "Valarie" };
+vector<string> names = { "<a:Enrique>", "<a:Arianne>", "<a:Muriel>", "<a:Santina>", "<a:Miguel>", "<a:Easter>", "<a:Arla>", "<a:Karisa>", "<a:Bailey>", "<a:Brice>", "<a:Della>", "<a:Pamala>", "<a:Mikel>", "<a:Roselyn>", "<a:Malisa>", "<a:Tommye>", "<a:Lucien>", "<a:Norma>", "<a:Fidela>", "<a:Loralee>", "<a:Waylon>", "<a:Ching>", "<a:Dewitt>", "<a:Saturnina>", "<a:Russel>", "<a:Helaine>", "<a:Gena>", "<a:Darlena>", "<a:Julianna>", "<a:Marylin>", "<a:Blanca>", "<a:Amee>", "<a:Adina>", "<a:Ashlea>", "<a:Louanne>", "<a:Merlyn>", "<a:Min>", "<a:Vivian>", "<a:Jaquelyn>", "<a:Mitchell>", "<a:Ka>", "<a:Suellen>", "<a:Jefferey>", "<a:Nicolette>", "<a:German>", "<a:Nisha>", "<a:Justine>", "<a:Debra>", "<a:Alycia>", "<a:Valarie>" };
 
 string r() {
 	return names[rand() % names.size()];
 }
 
-string cf = " goesTo theCoffee ";
-string mv = " goesTo theMovie ";
+string cf = " <a:goesTo> <a:theCoffee> ";
+string mv = " <a:goesTo> <a:theMovie> ";
 
 string o() {
 	if (rand() % 2) return cf;
 	return mv;
 }
 
-int main() {
+bool n3;
+
+ostream& p(string s, string po, string c = "") {
+	if (n3) cout << '{';
+	cout << s << po<<' ';
+	if (n3) cout << '}';
+	else cout<<c;
+	return cout;
+}
+
+int main(int argc, char** argv) {
+	n3 = argc == 2;
 	srand(time(0));
 	random_shuffle(names.begin(), names.end());
 	uint sz = names.size();
-	for (uint k = 0; k < 25; ++k) 
+	for (uint k = 0; k < 1; ++k) 
 		for (uint n = 0; n < sz; ++n) {
 			string s = names[n];
-			random_shuffle(s.begin(), s.end());
+			random_shuffle(s.begin() + 3, --s.end());
 			names.push_back(s);
 		}
 
 	for (uint n = 0; n < names.size(); ++n) {
 		string w = o();
-		cout << names[n] << w << string("A_") + names[n] << "."<<endl;
-		cout << r() << w << string("B_") + names[n] << "."<<endl;
+		p(names[n], w, string("A_") + names[n]) << (n3 ? "<=" : ".\n");
 		for (uint k = 0; k < sz; ++k)
-			cout << r() << o() << string("B_") + names[n] << "."<<endl;
-		cout << string("B_") + names[n] << " => " << string("A_") + names[n] << '.' << endl;
+			p( r(), o(), string("B_") + names[n] ) << "." <<endl;
+		if (!n3) cout << string("B_") + names[n] << " => " << string("A_") + names[n] << '.' << endl;
 	}
-	for (uint n = 0; n < sz; ++n) 
+	for (uint n = 0; n < sz/4; ++n) 
 		cout << r() << o() << '.' << endl;
-	cout << "?x" << cf <<"T1."<<endl;
-	cout << "?x" << mv <<"T1."<<endl;
-	cout << "?x staysAt Home T2."<<endl;
-	cout << "T1 => T2."<<endl;
-	cout<<"fin."<<endl<<"?y staysAt Home."<<endl<<"fin."<<endl;
+	if (n3)
+		cout << "{?x" << cf <<". ?x" << mv <<"}=>{?x <a:staysAt> <a:Home>}."<<endl;
+	else {
+		cout << "?x" << cf <<"T1."<<endl;
+		cout << "?x" << mv <<"T1."<<endl;
+		cout << "?x staysAt Home T2."<<endl;
+		cout << "T1 => T2."<<endl;
+		cout<<"fin."<<endl<<"?y staysAt Home."<<endl<<"fin."<<endl;
+	}
 }
