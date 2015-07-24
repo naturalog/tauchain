02c7291a6b127b25cf09dac4277ada3b317b4bd3
recycled consciousness <you@example.com>
2015-07-24 23:12:12 +0200

    Revert "disable builtins"
    This reverts commit 3deb18f42b4d47c8257cf52695811a39ec49c0ec.

--- prover.cpp
+++ prover.cpp
@@ -332,7 +332,7 @@ void prover::step(std::shared_ptr<proof> p, std::deque<shared_ptr<proof>>& queue
 		if (euler_path(p, queue)) return;
 		termid t = kb.body()[p->rul][p->last];
 		TRACE(dout<<"Tracking back from " << format(t) << std::endl);
-//		if (builtin(t, p, queue) != -1) return;
+		if (builtin(t, p, queue) != -1) return;
 		for (auto rl : kb[get(t).p]) {
 			std::shared_ptr<subst> s = sub();
 			if (unify(t, p->s, kb.head()[rl], s, true)) {
