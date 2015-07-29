79136b85cd09b6a3bf15309110f1707b08c86c26
naturalog <ohad@idni.org>
2015-07-29 22:29:05 +0300

    order match better now

--- prover.cpp
+++ prover.cpp
@@ -365,7 +365,7 @@ void prover::step(shared_ptr<proof>& _p, queue_t& queue, queue_t& gnd) {
 			if (unify(t, *p.s, kb.head()[rl], s, true)) {
 				shared_ptr<proof> r = make_shared<proof>(rl, 0, _p, s, p.g);
 				if (kb.body()[rl].empty()) r->g.emplace_back(rl, (shared_ptr<subst>)0);
-				queue.push_back(r);
+				queue.push_front(r);
 //				step(r, queue, gnd);
 			}
 			s.clear();
@@ -379,7 +379,7 @@ void prover::step(shared_ptr<proof>& _p, queue_t& queue, queue_t& gnd) {
 		if (!kb.body()[rl].empty()) r->g.emplace_back(rl, p.s);
 		unify(kb.head()[rl], p.s, kb.body()[r->rul][r->last], r->s = make_shared<subst>(*p.prev->s), true);
 		++r->last;
-		queue.push_front(r);
+		queue.push_back(r);
 //		step(r, queue, gnd);
 	}
 }
