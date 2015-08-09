354fe5f6cdf937ae1b77c96d82a5d5514205c4f3
naturalog <ohad@idni.org>
2015-08-09 20:19:49 +0300

    now dimacs2tau is fine, see examples/factoring.dimacs

--- dimacs2tau.cpp
+++ dimacs2tau.cpp
@@ -8,17 +8,13 @@ using namespace std;
 
 string var(int v) {
 	stringstream ss;
-	if (v > 0) ss << "?x" << v;
-	else ss << "?nx" << (-v);
+	if (v > 0) ss << "?p" << v;
+	else ss << "?n" << (-v);
 	return ss.str();
 }
 
-string clause(vector<int> c) {
+string clause(const vector<int>& c) {
 	stringstream ss;
-	if (c.size() == 1) {
-		ss << '('<<var(c[0])<<" F) or T.";
-		return ss.str();
-	}
 	ss << '(';
 	for (auto x : c) ss << var(x) << ' ';
 	ss << ") or T.";
@@ -32,6 +28,7 @@ string negs(int n) {
 }
 
 int main() {
+	int N = 0;
 	string s;
 	vector<vector<int>> cnf;
 	while (getline(cin, s)) {
@@ -41,12 +38,17 @@ int main() {
 		int x, y, z;
 		sscanf(s.c_str(), "%d %d %d", &x, &y, &z);
 		clause.push_back(x);
-		clause.push_back(y);
-		clause.push_back(z);
+		N = max(N, x);
+		if (y) {
+			clause.push_back(y);
+			N = max(N, y);
+			if (z) {
+				clause.push_back(z);
+				N = max(N, z);
+			}
+		}
 	}
-	int N = 0;
-	cout << "T not F.\nF not T.\n(T T T) or T.\n(T T F) or T.\n(T F T) or T.\n(T F F) or T.\n(F T T) or T.\n(F T F) or T.\n(F F T) or T.\n{ ";
-	for (auto x : cnf) for (auto y : x) if (abs(y)>N) N = abs(y);
+	cout << "T not F.\nF not T.\n(T) or T.\n(T T) or T.\n(T F) or T.\n(F T) or T.\n(T T T) or T.\n(T T F) or T.\n(T F T) or T.\n(T F F) or T.\n(F T T) or T.\n(F T F) or T.\n(F F T) or T.\n{ ";
 	for (auto x : cnf) cout << clause(x) << endl;
 	//cout << "fin." << endl << negs(N) << "fin." << endl;
 	cout << negs(N) << " } => { CNF a SAT }.\nfin.\n";
