4d162e674fd37c4caa346d01b44b623c23763df5
koo5 <running.sun@gmail.com>
2015-08-12 17:37:34 +0200

    ..

--- build-libmarpa.sh
+++ build-libmarpa.sh
@@ -5,7 +5,8 @@ sudo apt-get install build-essential autoconf automake libtool
 git submodule init;
 git submodule update;
 cd libmarpa-dist/dist;
-autoreconf
+rm ./configure;
+autoreconf;
 ./configure;
 make  
 
