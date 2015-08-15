0829d956eb4add25e67c9de3f11fc241804f8ac2
koo5 <running.sun@gmail.com>
2015-08-15 02:40:55 +0200

    gitlab CI

--- build-libmarpa.sh
+++ build-libmarpa.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo apt-get install build-essential autoconf automake libtool
+apt-get install  -y build-essential autoconf automake libtool
 
-git submodule init;
-git submodule update;
-cd libmarpa-dist/dist;
-rm ./configure;
-autoreconf;
-./configure;
+git submodule init
+git submodule update
+cd libmarpa/dist
+rm ./configure
+autoreconf
+./configure
 make  
 
