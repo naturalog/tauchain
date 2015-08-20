faeb3e2c267d7a774bcc5412995adc6b72bb96e0
naturalog <ohad@idni.org>
2015-08-20 14:29:00 +0300

    robust unifier

---
+++ unif.cpp
@@ -0,0 +1,64 @@
+#include <map>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+typedef long i64;
+typedef map<i64, i64> subs;
+
+void print(i64* t, size_t sz) {
+	while (sz--) cout << (*t++) << ' ';
+	cout << endl;
+}
+
+bool unify(i64* _s, i64* _d, size_t sz, subs& ssub, subs& dsub) {
+	i64 __s[sz], __d[sz];
+	i64 *s = __s, *d = __d;
+	memcpy(s, _s, sz * 8);
+	memcpy(d, _d, sz * 8);
+	i64 *end = &s[sz];
+	subs::const_iterator it;
+
+	auto dosub = [&](i64 x, i64 y, bool loop = true) {
+		cout << "begin " << x << ' ' << y << endl; print(__s,5); print(__d,5);
+		for (size_t n = 0; n < sz; ++n) {
+			if (__s[n] == x) __s[n] = y;
+			if (__d[n] == x) __d[n] = y;
+		}
+		cout << "end" << endl; print(__s,5); print(__d,5);
+	};
+
+	auto sub = [&](i64 x, i64 y) {
+		it = ssub.find(x);
+		if (it != ssub.end() && it->second != y && it->second > 0)
+			return false;
+		ssub.emplace(x, y);
+		dsub.emplace(x, y);
+		cout << "ssub: "; for (auto x : ssub) cout << x.first << ' ' << x.second << endl;
+		dosub(x, y, false);
+		return true;
+	};
+
+	for (auto x : ssub) dosub(x.first, x.second);
+
+	while (s != end) {
+		if (*s < 0 && *s > *d && !sub(*s, *d)) return false;
+		if ((*s < 0 || *d < 0) && !sub(*d, *s)) return false;
+		if (*s++ != *d++) return false;
+		cout << "***" << endl;print(__s,5); print(__d,5);
+	}
+
+	return true;
+}
+
+int main() {
+	i64 t1[] = { -1, 2, 3, 4, 5 };
+	i64 t2[] = { -9, 2, 3, -9, 5 };
+	subs s,d;
+	if (unify(t1, t2, 5, s, d)) cout << "passed" << endl;
+	else cout << "failed" << endl;
+	cout << "S:" << endl;
+	for (auto x : s) cout << x.first << ' ' << x.second << endl;
+	cout << "D:" << endl;
+	for (auto x : d) cout << x.first << ' ' << x.second << endl;
+}
