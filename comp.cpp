5de60ef9a8a596cd37f9a5bf5be0f3accdc6b8b3
naturalog <ohad@idni.org>
2015-09-04 19:37:45 +0300

    more comp.cpp

--- comp.cpp
+++ comp.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 typedef std::function<bool(int&, bool)> atom;
-typedef std::function<bool(atom&, atom&)> comp;
+typedef std::function<bool(int&, int&)> comp;
 
 atom compile_atom(int p) {
 	int val = p, state = 0;
@@ -21,7 +21,8 @@ atom compile_atom(int p) {
 	else u = [p, state](int& x) mutable { return x == p; };
 
 	return [u, val, state](int& x, bool unify) mutable {
-		if (unify) return u(x);
+		if (unify)
+			return u(x);
 		if (!state) return x = val, (bool)(state = 1);
 		return false;
 	};
@@ -29,17 +30,25 @@ atom compile_atom(int p) {
 
 comp compile_triple(atom s, atom o) {
 	bool state = false;
-	return [s, o, state](atom& _s, atom& _o) mutable {
-		if (!state) return _s = s, _o = o, state = true;
-		return state = false;
+	return [s, o, state](int& _s, int& _o) mutable {
+		switch (state) {
+		case 0: while (s(_s, false)) {
+				while (o(_o, false)) {
+					return (bool)(state = 1);
+		case 1:			;
+				}
+			}
+			state = 2;
+		}
+		return false;
 	};
 }
 
-comp nil = [](atom&, atom&) { return false; };
+comp nil = [](int&, int&) { return false; };
 
 comp compile_triples(comp f, comp r) {
 	uint state = 0;
-	return [f, r, state](atom& s, atom& o) mutable {
+	return [f, r, state](int& s, int& o) mutable {
 		switch (state) {
 		case 0: while (f(s, o)) {
 				return (bool)(state = 1);
@@ -56,9 +65,9 @@ comp compile_triples(comp f, comp r) {
 	};
 }
 
-comp compile_match(comp x, comp y, atom a) {
+comp compile_match(comp x, comp y, int& a) {
 	uint state = 0;
-	return [x, y, a, state](atom& s, atom& o) mutable {
+	return [x, y, a, state](int& s, int& o) mutable {
 		switch (state) {
 		case 0: while (x(a, o)) {
 				while (y(s, a)) {
@@ -85,16 +94,15 @@ ostream& operator<<(ostream& os, atom a) {
 }
 
 ostream& operator<<(ostream& os, comp p) {
-	atom s, o;
-	int ss, oo;
+	int s = 0, o = 0;
 	while (p(s, o))
-		os << s << o << endl;
+		os << s << ' ' << o << endl;
 	return os;
 }
 
 int main() {
 	comp kb = nil;
-	atom socrates = compile_atom(1);
+/*	atom socrates = compile_atom(1);
 //	atom A = compile_atom(2);
 	atom man = compile_atom(2);
 	atom mortal = compile_atom(3);
@@ -103,9 +111,18 @@ int main() {
 	kb = fact(x, man, kb);
 	kb = fact(x, mortal, kb);
 //	kb = fact(1, 2, kb);
+*/
+	atom x = compile_atom(1);
+	atom y = compile_atom(2);
+	atom z = compile_atom(3);
+	atom v = compile_atom(-1);
+	comp t1 = compile_triple(x, y);
+	comp t2 = compile_triple(v, y);
+	comp t3 = compile_triple(z, z);
+	kb = compile_triples(t1, compile_triples(t2, compile_triples(t3, nil)));
+	int ii = 1;
+	comp m = compile_match(kb, kb, ii);
 	cout << kb << endl;
-	atom a, s, o;
-	comp m = compile_match(kb, kb, a);
-	int aa;
+	int s, o;
 	while (m(s, o)) cout << s << o << endl;
 }
