1272d71937793b40d5de7767f8edc22b893e7ff0
naturalog <ohad@idni.org>
2015-09-13 18:42:03 +0300

    some organize

--- p.cpp
+++ p.cpp
@@ -1,4 +1,313 @@
-#include "p.h"
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <set>
+
+typedef std::wstring string;
+typedef int resid;
+struct term;
+
+#define vec_t vector<T,ispod>
+#define map_t map<K, V, ispod>
+#define vec_templ template<typename T, bool ispod>
+#define map_tmpl template<typename K, typename V, bool ispod>
+
+const size_t chunk = 4;
+
+template<typename T, bool ispod /*= std::is_pod<T>::value*/>
+struct vector {
+protected:
+	T* a;
+	static const size_t szchunk;
+	size_t n, c;
+public:
+	vector();
+	vector(const vector<T, ispod>& t);
+	vector<T, ispod>& operator=(const vector<T, ispod>& t);
+	~vector();
+
+	typedef T* iterator;
+	T operator[](size_t k) const;
+	size_t size() const;
+	T* begin() const;
+	T* end() const;
+	void clear();
+	void clear1(); // dont free mem
+	bool empty() const;
+	iterator back();
+	T& push_back(const T& t);
+
+	struct coro {
+		bool state;
+		iterator i, e;
+		const vector<T, ispod>* t;
+		coro(const vector<T, ispod>& _t) : state(false), i(0), e(0), t(&_t) {}
+		bool operator()() {
+			switch (state) {
+			case false: 
+				i = t->begin(), e = t->end();
+				while (i != e) {
+					return state = true;
+			case true: 	++i;
+				}
+				return state = false;
+			}
+		}
+	} m;
+
+protected:	
+	void copyfrom(const vector<T, ispod>& t);
+};
+
+template<typename K, typename V>
+struct mapelem {
+	K first;
+	V second;
+	mapelem(){}
+	mapelem(const mapelem& m) : first(m.first), second(m.second) {}
+	mapelem(const K& _k, const V& _v) : first(_k), second(_v) {} 
+};
+
+template<typename K, typename V, bool ispod>
+struct map : public vector<mapelem<K, V>, ispod > {
+	typedef mapelem<K, V> vtype;
+	typedef vector<mapelem<K, V>, ispod > base;
+	typedef map<K, V, ispod > _this;
+public:
+	map(const base& t);
+	map();
+	map(const _this& _s);
+	_this& operator=(const _this& m);
+	V& operator[](const K& k);
+	V& set(const K& k, const V& v);
+	typedef vtype* iterator;
+	iterator find(const K& k) const;
+private:
+	static int compare(const void* x, const void* y) { return ((vtype*)x)->first - ((vtype*)y)->first; }
+};
+
+class bidict {
+	map<resid, string, false> ip;
+	map<string, resid, false> pi;
+public:
+	resid set(string v);
+	string operator[](resid k);
+	resid operator[](string v);
+};
+
+vec_templ vec_t::vector() : a(0),n(0),c(0),m(*this) {}
+map_tmpl map<K, V, ispod>::map() : base() {}
+vec_templ vec_t::vector(const vector<T, ispod>& t) : a(0),n(0),c(0),m(*this) { copyfrom(t); }
+vec_templ vec_t& vector<T, ispod>::operator=(const vector<T, ispod>& t) { copyfrom(t); return *this; }
+vec_templ T vec_t::operator[](size_t k) const 		{ return a[k]; }
+vec_templ size_t vec_t::size() const			{ return n; }
+vec_templ T* vec_t::begin() const			{ return a; }
+vec_templ T* vec_t::end() const				{ return n ? &a[n] : 0; }
+vec_templ void vec_t::clear()				{ n = c = 0; if (!a) return; free(a); a = 0; }
+vec_templ void vec_t::clear1() 				{ n = c = 0; }
+vec_templ bool vec_t::empty() const			{ return !n; }
+vec_templ vec_t::~vector()				{ if(a)free(a); }
+vec_templ T* vec_t::back()				{ return n ? &a[n-1] : 0; }
+map_tmpl map<K, V, ispod>::map(const base& t)		{ copyfrom(t); }
+map_tmpl map<K, V, ispod>::map(const _this& _s) : base(){ base::copyfrom(_s); }
+
+map_tmpl map<K, V, ispod>& map<K, V, ispod>::operator=(const _this& m)	{ base::copyfrom(m); return *this; }
+map_tmpl V& map<K, V, ispod>::operator[](const K& k){ mapelem<K, V>* z = find(k); return z ? z->second : set(k, V()); }
+
+vec_templ T& vec_t::push_back(const T& t) {
+	if (!(n % chunk))
+		a = (T*)realloc(a, szchunk * ++c);
+	if (ispod) a[n] = t;
+	else new (&a[n])(T)(t);
+	return a[n++];
+}
+vec_templ void vec_t::copyfrom(const vector<T, ispod>& t) {
+	if (!(n = t.n)) { c = 0; return; }
+	memcpy(a = (T*)realloc(a, t.c * szchunk), t.a, (c = t.c) * szchunk);
+}
+
+template<typename T, bool ispod /*= std::is_pod<T>::value*/>
+const size_t vector<T, ispod>::szchunk = chunk * sizeof(T);
+
+map_tmpl V& map<K, V, ispod>::set(const K& k, const V& v) {
+	vtype* z = find(k);
+	if (z) { return z->second = v; }
+	return base::push_back(vtype(k, v)).second;
+//	qsort(base::a, base::n, sizeof(vtype), compare);
+}
+map_tmpl mapelem<K, V>* map<K, V, ispod>::find(const K& k) const {
+	if (!base::n) return 0;
+	iterator e = base::end();
+	for (vtype* x = base::begin(); x != e; ++x) if (x->first == k) return x;
+	return 0;
+//	vtype v(k, V()); vtype* z = (vtype*)bsearch(&v, base::a, base::n, sizeof(vtype), compare); return z;
+}
+//map_tmpl int map<K, V, ispod>::compare(const void* x, const void* y) { return ((vtype*)x)->first - ((vtype*)y)->first; }
+typedef vector<term*, true> termset;
+
+class nqparser {
+private:
+	wchar_t *t;
+	const wchar_t *s;
+	int pos;
+public:
+	nqparser();
+	~nqparser();
+
+	bool readcurly(vector<term*, true>& ts);
+	term* readlist();
+	term* readiri();
+	term* readbnode();
+	term* readvar();
+	term* readlit();
+	term* readany(bool lit = true);
+	void addhead(vector<term*, true>& ts, term* t, termset* subjs = 0);
+	vector<term*, true> operator()(const wchar_t* _s);
+	static vector<term*, true> readterms (std::wistream& is);
+};
+
+resid bidict::set ( string v ) {
+	if (!v.size()) throw "bidict::set called with a node containing null value";
+	map<string, resid, false>::iterator it = pi.find ( v );
+	if ( it ) return it->second;
+	resid k = pi.size() + 1;
+	if ( v[0] == L'?' ) k = -k;
+	pi[v] = k, ip[k] = v;
+	return k;
+}
+
+void trim(string& s) {
+	string::iterator i = s.begin();
+	while (iswspace(*i)) {
+		s.erase(i);
+		i = s.begin();
+	}
+	size_t n = s.size();
+	if (n) {
+		while (iswspace(s[--n]));
+		s = s.substr(0, ++n);
+	}
+}
+string wstrim(string s) { trim(s); return s; }
+string bidict::operator[](resid k) { return ip[k]; }
+resid bidict::operator[](string v) { return set(v); }
+
+
+typedef struct frame* pframe;
+const wchar_t endl[3] = L"\r\n";
+
+#ifdef DEBUG
+#define TRACE(x) x
+#else
+#define TRACE(x)
+#endif
+
+typedef map<resid, term*, true> subs;
+string format(const term* t, bool body = false);
+string format(const termset& t, int dep = 0);
+string format(const subs& s);
+void trim(string& s);
+string wstrim(string s);
+
+term* mkterm(termset& kb, termset& query);
+term* mkterm(resid p);
+term* mkterm(resid p, const termset& args);
+typedef term* (*fevaluate)(term&);
+typedef term* (*fevaluates)(term&, const subs&);
+typedef bool (*funify)(term& s, term& d, subs& dsub);
+typedef bool (*funify_ep)(term& s, term& d, const subs& dsub);
+
+term* evvar(term&);
+term* evnoargs(term&);
+term* ev(term&);
+term* evs(term&,const subs&);
+term* evvars(term&,const subs&);
+term* evnoargss(term&,const subs&);
+bool u1(term& s, term& d, subs& dsub);
+bool u2(term& s, term& d, const subs& dsub);
+bool u3(term& s, term& d, subs& dsub);
+bool u4(term& s, term& d, const subs& dsub);
+
+struct term {
+//	term();
+	term(termset& kb, termset& query);
+	term(resid _p, const termset& _args = termset());
+
+	const resid p;
+	termset args;
+	const size_t szargs;
+	termset matches, body;
+
+	fevaluate evaluate;
+	fevaluates evaluates;
+	funify unify;
+	funify_ep unify_ep;
+
+	void trymatch(termset& heads);
+
+	struct coro {
+		term& t;
+		term* i;
+		termset::coro match_coro, args_coro;
+		subs dsub;
+		bool state;
+		coro(term& _t) : t(_t), match_coro(t.matches), args_coro(t.args), state(false) {}
+		bool operator()() {
+			switch (state) {
+			case false:
+				while (match_coro()) {
+					{
+						termset::coro match_args_coro((*match_coro.i)->args);
+						while (args_coro() && match_args_coro())
+							if (!(*args_coro.i)->unify(**args_coro.i, **match_args_coro.i, dsub))
+								break;
+					}
+					if (!args_coro.state) {
+						i = *match_coro.i;
+						return state = true;
+					} else args_coro.state = false;
+			case true:
+					dsub.clear1();
+				}
+				return state = false;
+			}
+			return false;
+		}
+	} m;
+};
+
+struct tcmp {
+	bool operator()(term* _x, term* _y) const {
+		term &x = *_x, &y = *_y;
+		if (x.szargs != y.szargs) return x.szargs < y.szargs;
+		if (x.p != y.p) return x.p < y.p;
+		for (termset::iterator i = x.args.begin(), e = x.args.end(), j = y.args.begin(); i != e; ++i, ++j) 
+			if ((*i)->p != (*j)->p)
+				return (*i)->p < (*j)->p;
+		return false;
+	}
+};
+std::set<term*, tcmp> terms;
+
+const size_t tchunk = 8192, nch = tchunk / sizeof(term);
+
+typedef vector<mapelem<term*, subs>, false > ground;
+typedef map<resid, vector<mapelem<term*, ground>, false >, false > evidence;
+
+struct frame {
+	term* rule;
+	termset::iterator b;
+	pframe prev, creator, next;
+	subs s;
+	int ref;
+	ground g() const; // calculate the ground
+	frame(pframe c, frame& p, const subs& _s);
+	frame(pframe c, term* r, termset::iterator l, pframe p);
+	frame(pframe c, term* r, termset::iterator l, pframe p, const subs&  _s);
+	void decref();
+};
+void prove(pframe _p, pframe& lastp);
 
 std::wistream &din = std::wcin;
 std::wostream &dout = std::wcout;
@@ -244,9 +553,10 @@ term* nqparser::readany(bool lit) {
 	return ((pn = readbnode()) || (pn = readvar()) || (lit && (pn = readlit())) || (pn = readlist()) || (pn = readiri()) ) ? pn : 0;
 }
 
-void nqparser::addhead(termset& ts, term* t) {
+void nqparser::addhead(termset& ts, term* t, termset* subjs) {
 	if (!t) throw 0;
 	if (!t->p) throw 0;
+	if (subjs) t->body = *subjs;
 	ts.push_back(t);
 }
 
@@ -296,8 +606,7 @@ termset nqparser::operator()(const wchar_t* _s) {
 					addhead(heads, mkterm(x->first->p, ts));
 				}
 		else for (termset::iterator o = objs.begin(); o != objs.end(); ++o) {
-			(*o)->body = subjs;
-			addhead(heads, *o);
+			addhead(heads, *o, &subjs);
 		}
 		if (*s == L'}') { ++s; return heads; }
 		preds.clear();
