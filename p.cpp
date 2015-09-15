fc11ec161c1aa87e247a212c74a60bd1e747ec7e
naturalog <ohad@idni.org>
2015-09-15 21:54:06 +0300

    p.cpp stripped to parser only

--- p.cpp
+++ p.cpp
@@ -230,51 +230,13 @@ bool u3(term& s, term& d, subs& dsub);
 bool u4(term& s, term& d, const subs& dsub);
 
 struct term {
-//	term();
 	term(termset& kb, termset& query);
 	term(resid _p, const termset& _args = termset());
 
 	const resid p;
 	termset args;
 	const size_t szargs;
-	termset matches, body;
-
-	fevaluate evaluate;
-	fevaluates evaluates;
-	funify unify;
-	funify_ep unify_ep;
-
-	void trymatch(termset& heads);
-
-	struct coro {
-		term& t;
-		term* i;
-		termset::coro match_coro, args_coro;
-		subs dsub;
-		bool state;
-		coro(term& _t) : t(_t), match_coro(t.matches), args_coro(t.args), state(false) {}
-		bool operator()() {
-			switch (state) {
-			case false:
-				while (match_coro()) {
-					{
-						termset::coro match_args_coro((*match_coro.i)->args);
-						while (args_coro() && match_args_coro())
-							if (!(*args_coro.i)->unify(**args_coro.i, **match_args_coro.i, dsub))
-								break;
-					}
-					if (!args_coro.state) {
-						i = *match_coro.i;
-						return state = true;
-					} else args_coro.state = false;
-			case true:
-					dsub.clear1();
-				}
-				return state = false;
-			}
-			return false;
-		}
-	} m;
+	termset body;
 };
 
 struct tcmp {
@@ -291,24 +253,6 @@ struct tcmp {
 std::set<term*, tcmp> terms;
 
 const size_t tchunk = 8192, nch = tchunk / sizeof(term);
-
-typedef vector<mapelem<term*, subs>, false > ground;
-typedef map<resid, vector<mapelem<term*, ground>, false >, false > evidence;
-
-struct frame {
-	term* rule;
-	termset::iterator b;
-	pframe prev, creator, next;
-	subs s;
-	int ref;
-	ground g() const; // calculate the ground
-	frame(pframe c, frame& p, const subs& _s);
-	frame(pframe c, term* r, termset::iterator l, pframe p);
-	frame(pframe c, term* r, termset::iterator l, pframe p, const subs&  _s);
-	void decref();
-};
-void prove(pframe _p, pframe& lastp);
-
 std::wistream &din = std::wcin;
 std::wostream &dout = std::wcout;
 bool startsWith ( const string& x, const string& y ) { return x.size() >= y.size() && x.substr ( 0, y.size() ) == y; }
@@ -319,108 +263,8 @@ bidict dict;
 const resid implies = dict.set(L"=>");
 const resid Dot = dict.set(L".");
 
-//term::term() : p(0), szargs(0) { throw 0; }
-term::term(termset& kb, termset& query) : p(0), szargs(0), body(query),m(*this) { trymatch(kb); }
-term::term(resid _p, const termset& _args) : p(_p), args(_args), szargs(args.size()),
-		evaluate(p < 0 ? evvar : !szargs ? evnoargs : ev),
-		evaluates(p < 0 ? evvars : !szargs ? evnoargss : evs),
-		unify(p < 0 ? u1 : u3),
-		unify_ep(p < 0 ? u2 : u4),m(*this) {}
-
-void term::trymatch(termset& heads) {
-	subs d, e;
-	for (termset::iterator _b = body.begin(); _b != body.end(); ++_b)
-		for (termset::iterator it = heads.begin(); it != heads.end(); ++it) {
-			term& b = **_b;
-			ssub = e;
-			if (b.unify(b, **it, d)) b.matches.push_back(*it);
-			d.clear();
-		}
-}
-
-term* evvar(term& t) {
-	subs::vtype* v = ssub.find(t.p);
-	return v ? v->second->evaluate(*v->second) : (term*)0; 
-}
-
-term* ev(term& t) {
-	static term *v;
-	termset ts;
-	termset::coro c(t.args);
-	while (c()) {
-		term& b = **c.i;
-		ts.push_back(((v = b.evaluate(b))) ? v : mkterm(b.p));
-	}
-	return mkterm(t.p, ts);
-}
-
-term* evvars(term& t, const subs& ss) {
-	subs::vtype* v = ss.find(t.p);
-	return v ? v->second->evaluates(*v->second, ss) : (term*)0; 
-}
-
-term* evs(term& t, const subs& ss) {
-	static term *v;
-	termset ts;
-	termset::coro c(t.args);
-	while (c()) {
-		term& b = **c.i;
-		ts.push_back(((v = b.evaluates(b, ss))) ? v : mkterm(b.p));
-	}
-	return mkterm(t.p, ts);
-}
-
-term* evnoargs(term& t) { return &t; }
-term* evnoargss(term& t, const subs&) { return &t; }
-
-#define FASTEVAL(x) (((x).p > 0 && !(x).szargs) ? (&x) : (x).evaluate(x))
-#define FASTEVALS(x,s) (((x).p > 0 && !(x).szargs) ? (&x) : (x).evaluates(x,s))
-
-bool u1(term& s, term& d, subs& dsub) {
-	static subs::vtype *v;
-	static term *e;
-	if (!(v = ssub.find(s.p))) return true;
-	term& vs = *v->second;
-	return ((e = FASTEVAL(vs))) ? e->unify(*e, d, dsub) : true;
-}
-
-bool u2(term& s, term& d, const subs& dsub) { 
-	static subs::vtype *v;
-	static term *e;
-	if (!(v = ssub.find(s.p))) return true;
-	term& vs = *v->second;
-	return ((e = FASTEVAL(vs))) ? e->unify_ep(*e, d, dsub) : true;
-}
-
-bool u3(term& s, term& d, subs& dsub) {
-	if (d.p < 0) {
-		subs::vtype* v = dsub.find(d.p);
-		term* e = v ? FASTEVALS(*v->second, dsub) : 0;
-		if (e) return s.unify(s, *e, dsub);
-		dsub.set(d.p, FASTEVAL(s));
-		return true;
-	}
-	if (s.p != d.p || s.szargs != d.args.size()) return false;
-	termset::coro c(s.args), dit(d.args);
-	while (c() && dit())
-		if (!(*c.i)->unify(**c.i, **dit.i, dsub)) 
-			return false;
-	return true;
-}
-
-bool u4(term& s, term& d, const subs& dsub) {
-	if (d.p < 0) {
-		subs::vtype* v = dsub.find(d.p);
-		term* e = v ? FASTEVALS(*v->second, dsub) : 0;
-		return e ? s.unify_ep(s, *e, dsub) : true;
-	}
-	if (s.p != d.p || s.szargs != d.args.size()) return false;
-	termset::coro c(s.args), dit(d.args);
-	while (c() && dit())
-		if (!(*c.i)->unify_ep(**c.i, **dit.i, dsub)) 
-			return false;
-	return true;
-}
+term::term(termset& kb, termset& query) : p(0), szargs(0), body(query) { }
+term::term(resid _p, const termset& _args) : p(_p), args(_args), szargs(args.size()) {}
 
 size_t bufpos = 0;
 char* buf = (char*)malloc(tchunk);
@@ -438,20 +282,6 @@ char* buf = (char*)malloc(tchunk);
 	if (it != terms.end()) return *it; \
 	bufpos++; \
 	return r; 
-/*
-term* mkterm() {
-	if (bufpos == nch) {
-		buf = (char*)malloc(tchunk);
-		bufpos = 0;
-	}
-	new (&((term*)buf)[bufpos])(term);
-	term* r = &((term*)buf)[bufpos];
-	std::set<term*>::iterator it = terms.find(r);
-	if (it != terms.end()) return *it;
-	bufpos++;
-	terms.insert(r);
-	return r;
-}*/
 term* mkterm(termset& kb, termset& query) { MKTERM2(kb, query); }
 term* mkterm(resid p, const termset& args) { MKTERM2(p, args); }
 term* mkterm(resid p) {
@@ -554,7 +384,7 @@ term* nqparser::readany(bool lit) {
 }
 
 void nqparser::addhead(termset& heads, term* h, termset* body) {
-	if (body) t->body = *body;
+	if (body) h->body = *body;
 	heads.push_back(h);
 }
 
@@ -603,9 +433,8 @@ termset nqparser::operator()(const wchar_t* _s) {
 					ts.push_back(*o);
 					addhead(heads, mkterm(x->first->p, ts));
 				}
-		else for (termset::iterator o = objs.begin(); o != objs.end(); ++o) {
+		else for (termset::iterator o = objs.begin(); o != objs.end(); ++o)
 			addhead(heads, *o, &subjs);
-		}
 		if (*s == L'}') { ++s; return heads; }
 		preds.clear();
 		subjs.clear();
@@ -668,82 +497,19 @@ string format(const termset& t, int dep) {
 		else
 			ss +=  L" a fact.";
 		ss += endl;
-		for (termset::iterator y = x->body.begin(); y != x->body.end(); ++y) {
+/*		for (termset::iterator y = x->body.begin(); y != x->body.end(); ++y) {
 			IDENT;
 			((ss += L"\t") += format(*y, true) += L" matches to heads:") += endl;
 			for (termset::iterator z = (*y)->matches.begin(); z != (*y)->matches.end(); ++z) {
 				IDENT;
 				ss += L"\t\t" + format(*z, true) + endl;
 			}
-		}
+		}*/
 	}
 	return ss;
 }
 
-ground frame::g() const { 
-	if (!creator) return ground();
-	ground r = creator->g();
-	termset empty;
-	frame& cp = *creator;
-	typedef mapelem<term*, subs> elem;
-	if (cp.b != cp.rule->body.end()) {
-		if (!rule->body.size())
-			r.push_back(elem(rule, subs()));
-	} else if (cp.rule->body.size())
-		r.push_back(elem(creator->rule, creator->s));
-	return r;	
-}
-frame::frame(pframe c, term* r, termset::iterator l, pframe p, const subs& _s)
-		: rule(r), b(l ? l : rule->body.begin()), prev(p), creator(c), next(0), s(_s), ref(0) { if(c)++c->ref;if(p)++p->ref; }
-frame::frame(pframe c, term* r, termset::iterator l, pframe p)
-		: rule(r), b(l ? l : rule->body.begin()), prev(p), creator(c), next(0), ref(0) { if(c)++c->ref;if(p)++p->ref; }
-frame::frame(pframe c, frame& p, const subs& _s) 
-		: rule(p.rule), b(p.b), prev(p.prev), creator(c), next(0), s(_s), ref(0) { if(c)++c->ref; if(prev)++prev->ref; }
-void frame::decref() { if (ref--) return; if(creator)creator->decref();if(prev)prev->decref(); delete this; }
-
 size_t steps = 0;
-
-void prove(pframe _p, pframe& lastp) {
-	if (!lastp) lastp = _p;
-	evidence e;
-	while (_p) {
-		if (++steps % 1000000 == 0) (dout << "step: " << steps << endl);
-		pframe ep = _p;
-		frame& p = *_p;
-		term* t = p.rule, *epr;
-		// check for euler path
-		ssub = p.s;
-		while ((ep = ep->prev)) {
-			if ((epr = ep->rule) == p.rule && t->unify_ep(*t, *epr, ep->s)) {
-				t = 0;
-				break;
-			}
-		}
-		if (!t);
-		else if (p.b != p.rule->body.end()) {
-			term::coro& m = (**p.b).m;
-			while (m())
-				lastp = lastp->next = pframe(new frame(_p, m.i, 0, _p, m.dsub));
-		}
-		else if (p.prev) { // if body is done, go up the tree
-			frame& ppr = *p.prev;
-			pframe r(new frame(_p, ppr, ppr.s));
-			p.rule->unify(*p.rule, **r->b++, r->s);
-			prove(r, lastp);
-		}
-		#ifndef NOTRACE
-		else {
-			term* _t; // push evidence
-			for (termset::iterator r = p.rule->body.begin(); r != p.rule->body.end(); ++r) {
-				if (!(_t = ((*r)->evaluate(**r)))) continue;
-				e[_t->p].push_back(mapelem<term*, ground>(_t, p.g()));
-				dout << "proved: " << format(_t) << endl;
-			}
-		}
-		#endif
-		pframe pf = _p; _p = p.next; pf->decref();
-	}
-}
 
 int main() {
 	termset kb = nqparser::readterms(din);
@@ -752,15 +518,15 @@ int main() {
 	// create the query term and index it wrt the kb
 	term* q = mkterm(kb, query);
 	// now index the kb itself wrt itself
-	for (termset::iterator it = kb.begin(); it != kb.end(); ++it)
-		(*it)->trymatch(kb);
+//	for (termset::iterator it = kb.begin(); it != kb.end(); ++it)
+//		(*it)->trymatch(kb);
 	kb.push_back(q);
 	TRACE(dout << "kb:" << endl << format(kb) << endl);
 
 	// create the initial frame with the query term as the frame's rule
-	pframe p(new frame(pframe(), q, 0, pframe(), subs())), lp = 0;
+//	pframe p(new frame(pframe(), q, 0, pframe(), subs())), lp = 0;
 	clock_t begin = clock(), end;
-	prove(p, lp); // the main loop
+//	prove(p, lp); // the main loop
 	end = clock();
 	dout << "steps: " << steps << " elapsed: " << (1000. * double(end - begin) / CLOCKS_PER_SEC) << "ms" << endl;
 
