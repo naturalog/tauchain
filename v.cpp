871258184df4655eadb9232474a844cc5086c2e0
naturalog <ohad@idni.org>
2015-10-03 12:05:44 +0300

    v.cpp: add occurs check

--- v.cpp
+++ v.cpp
@@ -26,6 +26,19 @@ vector<rule> rules;
 
 typedef map<resource*, resource*> vars;
 
+bool occurs_check(resource *x, resource *y) {
+	if (x->type != VAR) {
+		if (y->type != VAR) return false;
+		return occurs_check(y, x);
+	}
+	if (y->type != LIST) return false;
+	resource **r = y->args;
+	while (*r)
+		if (occurs_check(x, *r))
+			return true;
+	return false;
+}
+
 bool compile(resource *s, resource *d, vars& c) {
 	if (s->type != VAR && d->type != VAR) {
 		if (s != d) return c.clear(), false;
@@ -36,6 +49,7 @@ bool compile(resource *s, resource *d, vars& c) {
 				return false;
 		return true;
 	}
+	if (occurs_check(s, d)) return false;
 	return c[s] = d, true;
 }
 
@@ -48,7 +62,7 @@ bool compile(triple *s, triple *d, vars& c) {
 
 // compiler's intermediate representation language. the information
 // in the following variable contains everything the emitter has to know
-map<tuple<int/*head*/,int/*body*/,int/*head*/>, vars> intermediate;
+map<pair<int/*head*/,int/*body*/>, map<int/*head*/, vars>> intermediate;
 
 void compile() {
 	vars c;
@@ -56,5 +70,9 @@ void compile() {
 		for (int k = 0; k < rules[n].body.size(); ++k)
 			for (int m = 0; m < rules.size(); ++m)
 				if (compile(&rules[n].body[k], &rules[m].head, c))
-					intermediate[make_tuple(n, k, m)] = c, c.clear();
+					intermediate[make_pair(n, k)][m] = c, c.clear();
 };
+
+void run() {
+	
+}
