5e40bee65f56ab7b7f2098c5d0bc61aa724f43f8
naturalog <ohad@idni.org>
2015-10-04 17:19:20 +0300

    v.cpp: some fixes

--- v.cpp
+++ v.cpp
@@ -17,38 +17,18 @@ wostream& dout = wcout;
 struct din_t { // due to wcin.peek() not working
 	wchar_t ch;
 	bool f = false;
-	wchar_t peek() {
-		if (f)
-			return ch;
-		return (f = true), (wcin >> ch), ch;
-	}
-	wchar_t get() {
-		if (f) 
-			return (f = false), ch;
-		return (wcin >> ch), ch;
-	}
+	wchar_t peek() { return f ? ch : ((f = true), (wcin >> ch), ch); }
+	wchar_t get() { return f ? ((f = false), ch) : ((wcin >> ch), ch); }
 	bool good() { return wcin.good(); }
-	void skip() {
-		while (good() && iswspace(peek()))
-			get();
-	}
-	wstring getline() {
-		wstring s;
-		f = false, std::getline(wcin, s);
-		return s;
-	}
+	void skip() { while (good() && iswspace(peek())) get(); }
+	wstring getline() { wstring s; f = false, std::getline(wcin, s); return s; }
 	din_t() { wcin >> noskipws; }
 } din;
 
-bool isdelim(wchar_t ch, wstring d = edelims) {
-	for (auto x : d) if (ch == x) return true;
-	return false;
-}
-
-wchar_t* till(/*wstring& d = edelims*/) {
+wchar_t* till() {
 	din.skip();
 	wstringstream ws;
-	while (!isdelim(din.peek()) && !iswspace(din.peek()))
+	while (din.good() && edelims.find(din.peek()) == string::npos && !iswspace(din.peek()))
 		ws << din.get();
 	wchar_t *r = wcsdup(ws.str().c_str());
 	return din.get(), (wcslen(r) ? r : 0);
@@ -58,30 +38,32 @@ wostream& operator<<(wostream& os, const struct res& r);
 wostream& operator<<(wostream& os, const struct triple& t);
 
 struct res {
-	wchar_t *val;
+	const wchar_t *val;
 	union {
-		res* sub;	// substitution, in case of var. not used in compile time
-		res** args;// list items. last item must be null
+		res* sub; // substitution, in case of var. not used in compile time
+		const res** args; // list items. last item must be null
 	};
-	res(wchar_t *v) : val(v ? wcsdup(v) : 0), sub(0) { }
+	res(const wchar_t *v) : val(v ? wcsdup(v) : 0), sub(0) { }
 	res(vector<res*> _args)
-		: val(new wchar_t[2]), args(new res*[_args.size() + 1]) {
-		*val= L'.', val[1] = 0;
+		: val(wcsdup(L".")), args(new const res*[_args.size() + 1]) {
 		int n = 0;
 		for (auto x : _args) args[n++] = x;
 		args[n] = 0;
 	}
-	~res() { if (args && *val == L'.') delete[] args; if (val) free(val); }
+	~res() { if (args && *val == L'.') delete[] args; if (val) free((wchar_t*)val); }
 };
 
-#define _mkres(T, K) \
-res* mkres(T v) { \
-	static map<K, res*> r; \
-	static map<K, res*>::iterator i; \
-	return ((i = r.find(K(v))) == r.end()) ? (r[v] = new res(v)) : i->second; \
+res* mkres(const wchar_t* v) { 
+	static map<wstring, res*> r; 
+	static map<wstring, res*>::iterator i; 
+	return ((i = r.find(wstring(v))) == r.end()) ? (r[v] = new res(v)) : i->second; 
 }
-_mkres(wchar_t*, wstring)
-_mkres(vector<res*>, vector<res*>)
+
+res* mkres(const vector<res*>& v) { 
+	static map<vector<res*>, res*> r; 
+	static map<vector<res*>, res*>::iterator i; 
+	return ((i = r.find(v)) == r.end()) ? (r[v] = new res(v)) : i->second; 
+}
 
 struct triple {
 	res *r[3]; // spo
@@ -128,7 +110,7 @@ wostream& operator<<(wostream& os, const triple& t) {
 wostream& operator<<(wostream& os, const res& r) {
 	if (*r.val == L'.') {
 		os << L'(';
-		res **a = r.args;
+		const res **a = r.args;
 		while (*a) os << (**a) << L' ';
 		os << L')'; 
 	} else {
@@ -137,7 +119,7 @@ wostream& operator<<(wostream& os, const res& r) {
 	}
 	return os;
 }
-void print() { for (auto r : rules) wcout << r << endl; } 
+void print() { dout << "rules: " << rules.size() << endl; for (auto r : rules) dout << r << endl; } 
 
 res* readany();
 
@@ -171,12 +153,12 @@ void readrule() {
 	triples body;
 	if (din.peek() == L'{') {
 		din.get();
-		while (din.peek() != L'}') body.push_back(readtriple());
+		while (din.good() && din.peek() != L'}') body.push_back(readtriple());
 		din.get(), din.skip(), din.get(),
 		din.get(), din.skip(), din.get(), din.skip(); // "} => {";
 		if (din.peek() == L'}') rules.push_back(rule(0, body)), din.skip();
 		else {
-			while (din.peek() != L'}') rules.push_back(rule(readtriple(), body));
+			while (din.good() && din.peek() != L'}') rules.push_back(rule(readtriple(), body));
 			din.get();
 		}
 	} else {
@@ -189,7 +171,7 @@ void readrule() {
 
 void readdoc() { while (din.good()) din.skip(), readrule(), din.skip(); }
 
-typedef map<res*, res*> subs;
+typedef map<const res*, const res*> subs; // for compile time only
 
 const size_t max_frames = 1e+6;
 struct frame {
@@ -204,22 +186,22 @@ typedef map<int/*head*/, subs> conds;
 map<int/*head*/,map<int/*body*/, conds>> intermediate;
 map<int/*head*/,map<int/*body*/, std::function<bool()>>> program; // compiled functions
 
-bool occurs_check(res *x, res *y) {
+bool occurs_check(const res *x, const res *y) {
 	if (!isvar(*x)) return isvar(*y) ? occurs_check(y, x) : false;
 	if (x == y) return true;
 	if (islist(*y))
-		for (res **r = y->args; *r; ++r)
+		for (const res **r = y->args; *r; ++r)
 			if (occurs_check(x, *r))
 				return true;
 	return false;
 }
 
-bool prepare(res *s, res *d, subs& c) {
+bool prepare(const res *s, const res *d, subs& c) {
 	dout << "preparing " << *s << " and " << *d << endl;
 	if (!isvar(*s) && !isvar(*d)) {
 		if (s != d) return dout << " failed." << endl, c.clear(), false;
 		if (!islist(*s)) return true;
-		res **rs, **rd;
+		const res **rs, **rd;
 		for (rs = s->args, rd = d->args; *rs && *rd;)
 			if (!*rs != !*rd || !prepare(*rs++, *rd++, c))
 				return false;
@@ -245,13 +227,13 @@ void prepare() {
 					intermediate[n][k][m] = c, c.clear();
 }
 
-bool unify(res *s, res *d, subs& trail) { // in runtime
+bool unify(const res *s, const res *d, subs& trail) { // in runtime
 }
 
 function<bool()> compile(conds& c) {
 	return [c]() {
 		for (conds::const_iterator x = c.begin(); x != c.end(); ++x) {
-			for (pair<res*, res*> y : x->second)
+			for (pair<const res*, const res*> y : x->second)
 				if (!unify(y.first, y.second, last->trail))
 					continue;
 			last->h = x->first, last->b = 0, last->prev = first, ++last;
