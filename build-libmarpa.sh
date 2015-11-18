e1258e546119b2d99e177d8dcb9c65ccdfc1f792
koo5 <running.sun@gmail.com>
2015-11-18 01:06:37 +0100

    gitlab

--- build-libmarpa.sh
+++ build-libmarpa.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+test -r libmarpa/dist/.libs/libmarpa.so && exit 0
 
-apt-get install  -y build-essential autoconf automake libtool
+apt-get update -q
+apt-get install -y build-essential autoconf automake libtool
 
 git submodule init
 git submodule update
