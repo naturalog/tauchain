aa34dfd028c68506a84a12c772fc14c2b9a73bb6
naturalog <ohad@idni.org>
2016-04-02 04:01:25 +0300

    mutation example, without validation

---
+++ vm/mutation_example.cpp
@@ -0,0 +1,25 @@
+// Positive integers are vars. Negatives are list length up
+// to max list length in kb, integers smaller than -max represent consts.
+// NOTE: Assuming de-bruijn. this is crucial!
+#include "cfpds.h"
+struct dna : public vector<pair<int, uf*>> { };
+struct rule : public vector<dna> {
+	rule(int nvars) : nvars(nvars) {}
+	const int nvars;
+	// mutation without validation
+	rule* rule::mutate( uf *c ) {
+		rule *r = new rule(nvars);
+		for (dna d : *this) // go over all body items
+			for (auto p : d) { // go over all body's head matches
+				uf *u;
+				// go over all vars
+				for (int n = 0; n < nvars; ++n) 
+					// merge uf
+					u = unio(p.second, n, find(c, n)); 
+				// push dna to new rule
+				r->push_back(make_pair(p.first, u)); 
+			}
+		return r;
+	}
+};
+struct kb : public vector<rule> {};
