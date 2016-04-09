8815af90956111e03861235dac6300fb58af378b
naturalog <ohad@idni.org>
2016-04-09 05:11:51 +0300

    mutation_example.cpp: frames

--- vm/mutation_example.cpp
+++ vm/mutation_example.cpp
@@ -20,40 +20,48 @@ struct match {
 	set<int> vars;
 };
 
-struct rule : private vector<vector<match*>*> {
+struct rule : public vector<vector<match*>*> {
 	rule* mutate(puf &c);
 	vector<rule*> mutations;
 	rule *prev; // in the mutation tree
 	pair<rule*, unsigned> up; // up the proof tree, rule and body item id
 };
 
-rule* rule::mutate(puf &c) {
+struct frame {
+	rule &r;
+	unsigned b;
+	frame(rule &r) : r(r), b(0) {}
+	frame* mutate(puf &c);
+};
+
+frame* frame::mutate(puf &c) {
 	rule &res = *new rule;
 	match *t;
-	for (vector<match*> *p : *this) { // per body item
-		auto &v = *new vector<match*>;
-		for (match *mt : *p) { // per matching head
-			int r;
-			match &m = *new match;
-			m.rl = mt->rl;
-			m.conds = mt->conds;
-			for (int x : mt->vars)
-				if (!m.conds->unio(x, r = c.find(x))) {
-					delete &m;
-					goto stop;
-				} else if (r > 0) // rep is var
-					m.vars.insert(x);
-			v.push_back(&m);
-		stop:
-		}
-		if (v.empty()) { // body item can't ever be satisfied
-			delete &res;
-			delete &v;
-			return 0;
-		}
-		res.push_back(&v);
+	vector<match*>* p = r[b];
+	auto &v = *new vector<match*>;
+	for (match *mt : *p) { // per matching head
+		int r;
+		match &m = *new match;
+		m.rl = mt->rl;
+		m.conds = mt->conds;
+		for (int x : mt->vars)
+			if (!m.conds->unio(x, r = c.find(x))) {
+				delete &m;
+				goto stop;
+			} else if (r > 0) // rep is var
+				m.vars.insert(x);
+		v.push_back(&m);
+	stop:	;
 	}
-	return mutations.push_back(&res), res.prev = this, &res;
+	if (v.empty()) { // body item can't ever be satisfied
+		delete &res;
+		delete &v;
+		return 0;
+	}
+	res.push_back(&v);
+	r.mutations.push_back(&res);
+	res.prev = &r;
+	return new frame(res);
 }
 
 struct kb_t : public vector<rule*> { } *kb;
