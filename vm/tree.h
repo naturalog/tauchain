d93ceb90a9f12b5c922ff0825c02664183d73415
naturalog <ohad@idni.org>
2016-05-08 17:30:07 +0300

    tree.h uf uuf term

--- vm/tree.h
+++ vm/tree.h
@@ -1,13 +1,16 @@
+#include <cstring>
 #include <deque>
 #include <functional>
+#include <map>
 using namespace std;
+
 template<typename T>
 class btree {
+protected:
 	T v;
 	btree<T> *r, *l, *u;
 	deque<btree<T>*> rev;
 	deque<pair<btree<T>*, btree<T>*>> corev;
-//	map<btree<T>*, unsigned> pos;
 	btree<T>* root();
 public:
 	btree(T v, btree<T> *r = 0, btree<T> *l = 0, btree<T> *u = 0)
@@ -68,15 +71,78 @@ void btree<T>::corevisit(btree<T>* x, btree<T>* y) {
 }
 
 template<typename T>
-bool btree<T>::covisit(covisitor v, btree<T> *t) {
-	return  !r == !t->r && !l == !t->l 	&&
+bool btree<T>::covisit(covisitor v, btree<T> *t) {	
+	return  t && !r == !t->r && !l == !t->l &&
 		(!r || r->covisit(v, t->r))	&&
 		v(this, t)			&&
 		(!l || l->covisit(v, t->r));
 }
 
+struct term_base {
+	const wchar_t *n;
+	void *g; // scope
+	bool isvar;
+	bool operator==(const term_base&) const;
+};
 
+struct var : public term_base {
+	unsigned deb; // ruijn
+	class term *t;
+};
 
+bool term_base::operator==(const term_base& x) const {
+	if (isvar) {
+		if (!x.isvar) return false;
+		return !memcmp(this, &x, sizeof(var));
+	}
+	if (x.isvar) return false;
+	return !memcmp(this, &x, sizeof(term_base));
+}
 
+#define newitem(x) new item(x)
 
+template<typename T>
+struct uf {
+	struct item {
+		T* t;
+		item *rep;
+		set<item*> next;
+		item(const T& t) : t(t), rep(0) {}
+	};
+	map<T, item*> m;
+	T rep(const T& t) {
+		auto it = m.find(t);
+		if (it == m.end()) return m[t] = newitem(t), t;
+		return rep(it->second)->t;
+	}
+private:
+	item *rep(const item *i) {
+		return  !i->rep		? i	:
+			!i->rep->rep	? i->rep:
+			(m[i->rep->t] = i->rep = rep(i->rep));
+	}
+};
 
+template<typename T>
+struct uuf : private uf<T> { // unification union find
+};
+
+class term : public btree<term_base*> {
+	typedef function<bool(term*, term*)> addcond;
+	typedef multimap<term*, term*> rawconds;
+
+	inline bool var() const { return v->isvar; }
+
+	bool get_rawconds(term *x, rawconds *r, uuf<int> *u) {
+		return covisit([r](btree<T>* x, btree<T>* y) mutable {
+			return	(x->var() && y->var())	?
+				u.merge(x, y)		:
+				(x->var() || y->var())	?
+				r->emplace(x, y), true 	:
+				(x->v == y->v && 
+				!x->l == !y->l && 
+				!x->r == !y->r);
+		}, x);
+	}
+public:
+}
