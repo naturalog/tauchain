e96497382921fcaa614c9e09b51f6223cae78f05
naturalog <ohad@idni.org>
2016-05-09 09:00:34 +0300

    misc

--- vm/e.c
+++ vm/e.c
@@ -9,17 +9,14 @@ term *terms;
 uint nterms, nvars, nuirs, *reps, *ranks;
 undo *un = 0;
 
-void push_change(uint x, uint y, bool rank) {
+void save(uint x, uint y, bool rank) {
 	undo *u = un;
-	un = malloc(sizeof(undo));
+	un = malloc(sizeof(undo)),
 	un->rank = rank, un->x = x, 
 	un->y = y, un->n = u;
 }
 
-termid update(termid x, termid y) {
-	push_change(x, y, true);
-	return reps[x] = y;
-}
+#define update(x, y) save(x, y, true), reps[x] = y
 
 void revert(undo *u) {
 	if (!u) {
@@ -39,38 +36,23 @@ void commit(undo *u) {
 }
 
 termid rep(termid t) {
-	return	reps[t] == t	?
-		t		:
-		update(t,rep(reps[t]));
+	return	reps[t] == t ? t : update(t,rep(reps[t]));
 }
 
-void inc_rank(termid x) {
-	push_change(x, ranks[x]++, true);
-}
-
-#define merge_cons(x, y, s) \
-	merge(x.r, y.r, s) && \
-	merge(x.l, y.l, s)
-
-#define merge_vars(x, y) (\
-	ranks[x] > ranks[y]	?  update(y, x)	: \
-	ranks[x] < ranks[y]	?  update(x, y)	: \
-	update(y, x)		,  inc_rank(x))
-
-bool merge(termid _x, termid _y, void *scope) {
+bool merge(termid _x, termid _y, void *scop) {
 	termid rx = rep(_x), ry = rep(_y);
 	if (rx == ry) return true;
 	term x = terms[rx], y = terms[ry];
 	assert(memcmp(&x, &y, sizeof(term)));
 
-	return	x.isvar && y.isvar	?
-		merge_vars(rx, ry), true:
+	return	x.isvar && y.isvar	?  
+		ranks[rx] > ranks[ry]	? update(ry, rx)	:
+		ranks[rx] < ranks[ry]	? update(rx, ry)	:
+		update(ry, rx)		, save(rx,ranks[rx]++,1):
 		!x.leaf && !y.leaf	?
-		merge_cons(x, y, scope)	:
-		x.isvar			?
-		update(rx, ry)		:
-		!y.isvar		?
-		false			:
+		merge(x.r, y.r, scop)	&& merge(x.l, y.l, scop):
+		x.isvar			?  update(rx, ry)	:
+		!y.isvar		?  false		:
 		update(ry, rx), true	;
 }
 
