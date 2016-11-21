29a4fe52212c4b37da87ff4599ecc77975f01ffb
naturalog <ohad@idni.org>
2016-11-21 06:14:40 +0100

    uf computation begins working

--- stlc/stlc.c
+++ stlc/stlc.c
@@ -5,92 +5,7 @@
 #include <string.h>
 #include <err.h>
 
-typedef struct term term;
-typedef struct type type;
 typedef struct tree tree;
-typedef struct var var;
-typedef struct sym sym;
-typedef struct app app;
-typedef struct abst abst;
-typedef struct rec rec;
-
-struct type {
-	bool isfunc;
-	union {
-		char o;
-		const type *c, *d;
-	};
-};
-struct var {
-	const char* name;
-	const type* t;
-};
-struct sym {
-	const char* name;
-	const type* t;
-};
-struct app {
-	const term *M, *N;
-};
-struct abst {
-	const var *x;
-	const term *M;
-};
-struct rec {
-	const var *x;
-	const term *M;
-};
-struct term {
-	char t;
-	union {
-		var x;
-		sym c;
-		app a;
-		abst l;
-		rec y;
-	};
-};
-
-term* mkvar(const char* name, const type* tt) {
-	term *t = malloc(sizeof(term));
-	return t->t = 'x', t->x.name = name, t->x.t = tt, t;
-}
-term* mksym(const char* name, const type* tt) {
-	term *t = malloc(sizeof(term));
-	return t->t = 'c', t->c.name = name, t->c.t = tt, t;
-}
-term* mkapp(const term* M, const term* N) {
-	term *t = malloc(sizeof(term));
-	return t->t = '@', t->a.M = M, t->a.N = N, t;
-}
-term* mkabst(const var* x, const term* M) {
-	term *t = malloc(sizeof(term));
-	return t->t = '\\', t->l.x = x, t->l.M = M, t;
-}
-term* mkrec(const var* x, const term* M) {
-	term *t = malloc(sizeof(term));
-	return t->t = 'Y', t->y.x = x, t->y.M = M, t;
-}
-const type* mktype(const type *c, const type *d) {
-	static type* base = 0;
-	if (!base) {
-		base = malloc(sizeof(type));
-		base->isfunc = false;
-	}
-	if (!d) return c ? c : base;
-	type *res = malloc(sizeof(type));
-	res->isfunc = true, res->c = c, res->d = d;
-	return res;
-}
-const type* type_infer(const term* t) {
-	switch (t->t) {
-	case '@': return mktype(type_infer(t->a.M), type_infer(t->a.N));
-	case '\\':return mktype(type_infer(t->l.M), 0);
-	case 'Y':return mktype(type_infer(t->y.M), 0);
-	case 'x':return t->x.t;
-	case 'c':return t->c.t;
-	}
-}
 struct tree {
 	const char* t;
 	tree *r, *l, *rep;
@@ -99,7 +14,8 @@ void printtree(tree* t);
 
 tree* uf_find(tree *t) {
 	tree *r = t->rep;
-	return t->rep = r ? uf_find(r) : t;
+	if (r) return t->rep = uf_find(r);
+	return t;
 }
 
 void uf_setrep(tree *r, tree *t) {
@@ -108,6 +24,7 @@ void uf_setrep(tree *r, tree *t) {
 }
 
 void uf_union(tree *x, tree *y) {
+	x = uf_find(x), y = uf_find(y);
 	if (!islower(*x->t)) uf_setrep(x, y);
 	else uf_setrep(y, x);
 }
@@ -120,6 +37,7 @@ void equiv(tree *x, tree *y) {
 }
 
 void compute(tree *t) {
+	if (!t) return;
 	if (*t->t == '\\') equiv(t->r, t->l);
 	else if (*t->t == '@') uf_union(t->l->l, t->r), 
 	compute(t->l);
@@ -130,7 +48,7 @@ tree* readtree(short d) {
 	char *w = malloc(4096), *r, *ww = w, c;
 	unsigned short p = 0;
 	tree *t = malloc(sizeof(tree));
-	t->r = t->l = 0;
+	t->r = t->l = t->rep = 0;
 	bool f = true;
 
 	while (f)
@@ -166,14 +84,28 @@ tree* readtree(short d) {
 }
 void printtree(tree* t) {
 	if (!t) return;
+//	t = uf_find(t);
+	printf("%s", t->t);
+	if (t->r) putchar('('), printtree(t->l), putchar(','), printtree(t->r), putchar(')');
+	fflush(stdin);
+}
+void print_computed(tree* t) {
+	if (!t) return;
+	switch (*t->t) {
+	case '\\': print_computed(t->r); return;
+	case '@': print_computed(t->l); return;
+	default: ;
+	}
+	t = uf_find(t);
 	printf("%s", t->t);
 	if (t->r) putchar('('), printtree(t->l), putchar(','), printtree(t->r), putchar(')');
 	fflush(stdin);
 }
 
 int main() {
-	tree *t;
-//	while (!feof(stdin))
-		 printtree(readtree(0)), puts("");
+	tree *t = readtree(0);
+	printtree(t), puts("");
+	compute(t);
+	print_computed(t), puts("");
 	return 0;
 }
